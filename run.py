452458c1453b419fb6f4a8bf932efd98ebe12191
Jack Pitz <jackpitz95@gmail.com>
2024-11-08 18:48:42 +0000

    Add string to generate numbers for columns

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 import random
 
-def create_board(size=6):
+def create_board(size=10):
     return[['0'] * size for _ in range(size)]
 
     def print_board(board):
         print("  " + " ".join(str(i) for i in range(len)(board)))
-        
+        for i, row in enumerate(board):
+            print(f"{i} " + " ".join(row))
+    
+
