b901eb2d2d60a63e9e55344256104512e3a6729d
Jack Pitz <jackpitz95@gmail.com>
2024-11-09 15:00:14 +0000

    Make further changes to create board function

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 import random
 
 def create_board(size):
-    return[['0' for _ in range (size)] for _ in range(size)]
+    for row in grid:
+        print(" ".join(row))
 
     def print_board(board):
         print("  " + " ".join(str(i) for i in range(len)(board)))
