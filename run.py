92b141241c0a8e3812b08913b2895688c8b6e634
Jack Pitz <jackpitz95@gmail.com>
2024-11-09 15:47:21 +0000

    Add get user guess function

--- run.py
+++ run.py
@@ -4,13 +4,18 @@ def print_grid(grid):
     for row in grid:
         print(" ".join(row))
 
+        
         def create_grid(size):
             grid = []
             for _ in range(size):
                 grid.append(["0"] * size)
                 return grid
 
+                
                 def place_computer_ship(size):
                     ship_row = random.randint(0, size - 1)
                     ship_col = random.randint(0, size  -1)
                     return (ship_row, ship_col)
+
+                    
+                    def get_user_guess():
