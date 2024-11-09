744f75f5857966f3f575643ac2c32fb1a738cfc6
Jack Pitz <jackpitz95@gmail.com>
2024-11-09 15:01:39 +0000

    Delete functions to make new ones

--- run.py
+++ run.py
@@ -1,12 +1,2 @@
 import random
 
-def create_board(size):
-    for row in grid:
-        print(" ".join(row))
-
-    def print_board(board):
-        print("  " + " ".join(str(i) for i in range(len)(board)))
-        for i, row in enumerate(board):
-            print(f"{i} " + " ".join(row))
-    
-
